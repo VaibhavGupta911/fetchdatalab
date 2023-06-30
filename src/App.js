4d2f39d4e25afde41e1c64fa063a5aceedb14e36
VaibhavGupta911 <guptaayush519@gmail.com>
2023-06-30 14:18:14 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [user, setUser] = React.useState([]);
+
+  const fetchData = () => {
+fetch(`https://randomuser.me/api/?results=1`)
+    .then((response)=>response.json())
+    .then((data)=>setUser(data))
+  };
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  return Object.keys(user).length > 0 ? (
+    <div style={{padding: "40px"}}>
+      <h1>Customer data</h1>
+      <h2>Name: {user.results[0].name.first} {user.results[0].name.last}</h2>
+      <img height={200} src={user.results[0].picture.large} alt="profile image"/>
+<h2>Date of Birth: {user.results[0].dob.date}</h2>
+<h2>Phone Number: {user.results[0].phone}</h2>
+<h2>Address: {user.results[0].location.street.number} {user.results[0].location.street.name} {user.results[0].location.city} {user.results[0].location.country}</h2>
     </div>
+  ) : (
+    <h1>Data pending...</h1>
   );
 }
 
